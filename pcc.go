f6eee5d67fa54adc140aed568791a3211fbf92d7
chenhao392 <chenhao.mymail@gmail.com>
2015-06-09 23:09:12 +0000

    update1

--- pcc.go
+++ pcc.go
@@ -1,4 +1,48 @@
-import "github.com/gonum/matrix/mat64"
+package main
+import ("github.com/gonum/matrix/mat64"
+	"bufio"
+	"fmt"
+	"flag"
+	"strings"
+	)
+
+
+func main(){
+	var inFile *string=flag("i","inMatrix.txt","tab delimited matrix")
+	data := raedFile(inFile)
+	//fmt.print(data)
+}
+func readFile(inFile string)(data [][]float64,error){
+	file, err := os.Open(inFile)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+	br :=bufio.NewReader(file)
+	data = [][]float64
+	for{
+		line,isPrefix,err1 := br.ReadLine()
+		if err1 != nil{
+			break
+		}
+		if isPrefix{
+			return
+		}
+		str=string(line)
+		elements :=strings.Split(str,"\t")
+		data=append(data,elements)
+	}
+	return data
+}
+
+
+// input: 2D array data in float64
+//output: pointer to * mat64.Dense to a  struc Dense
+//Dense is a dense matrix representation.
+//type Dense struct {
+// 	mat blas64.General
+//  	capRows, capCols int
+//}
 func cov(data ...[]float64) (*mat64.Dense, error) {
 	nSets := len(data)
 	if nSets == 0 {
@@ -10,7 +54,9 @@ func cov(data ...[]float64) (*mat64.Dense, error) {
 			return nil, errors.New("cov: datasets have unequal size")
 		}
 	}
+	//new Dense struc with row and columns defined as nSets, nil is for empty mat
 	covmat := mat64.NewDense(nSets, nSets, nil)
+	//slice function make to generate an array slice with length nSets
 	means := make([]float64, nSets)
 	for i := range means {
 		means[i] = floats.Sum(data[i]) / float64(nData)
@@ -28,4 +74,5 @@ func cov(data ...[]float64) (*mat64.Dense, error) {
 			covmat.Set(j, i, cv)
 		}
 	}
+	return covmat
 }
